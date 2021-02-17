e07022e7bfb6590c28a9855dffb40d70b9f346e1
Nisha <nishashukla1987@gmail.com>
2021-02-17 14:08:21 +0100

    fixed update

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,8 +1,8 @@
 <template>
 <div class="input-group d-flex mt-2">
-  <span class="flex-grow-1 input-group-text">{{todo}} </span>  
 
   <input v-if="edit" class="form-control flex-grow-1" type="text" v-model="update" placeholder="update me" />
+  <span v-else class="flex-grow-1 input-group-text">{{todo}} </span>  
   <div class="input-group-append">
   <button class="btn btn-danger" type="button" v-on:click="deleteTodo(index)">Delete</button>
   <button class="btn btn-warning" type="button" v-on:click="editTodoList()">Update</button>
@@ -23,7 +23,7 @@ export default {
     },
     data(){
         return {
-        update:'',
+        update:this.todo,
         edit:false
         
         }
